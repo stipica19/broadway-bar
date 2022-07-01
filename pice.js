f47ba85f88a01c158aafb0df74780536e8af5453
stipica19 <pipiklepic1@gmail.com>
2022-07-01 12:48:10 +0200

    nove cijene

---
+++ pice.js
@@ -0,0 +1,402 @@
+export let hotDrinks = [
+  {
+    naziv: 'Kava',
+    eng: 'Coffe',
+    kolicina: '',
+    cijena: '1,50 KM',
+  },
+  {
+    naziv: 'Nesscaffe vanilija čokolada',
+    eng: 'Nesscaffe vaniilla chocolate',
+    kolicina: '',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Nescafe 3in1',
+    eng: '',
+    kolicina: '',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Kapučino',
+    eng: 'Cappuccino',
+    kolicina: '',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Nescaffe',
+    eng: 'Nescaffe',
+    kolicina: '',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Čaj',
+    eng: 'Tea',
+    kolicina: '',
+    cijena: '1,50 KM',
+  },
+  {
+    naziv: 'Topla čokolada',
+    eng: 'Hot Chocolate',
+    kolicina: '',
+    cijena: '2,50 KM',
+  },
+];
+
+export let softDrinks = [
+  {
+    naziv: 'Prirodni sokovi',
+    eng: 'natural juices',
+    kolicina: '0,20l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Gazirani sokovi',
+    eng: 'Carbonated juice',
+    kolicina: '0,25l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Orangina',
+    eng: '',
+    kolicina: '0,25l',
+    cijena: '3,00 KM',
+  },
+  {
+    naziv: 'Eisberg',
+    eng: '',
+    kolicina: '0,25l',
+    cijena: '3,50 KM',
+  },
+  {
+    naziv: 'Red Bull',
+    eng: '',
+    kolicina: '0,25l',
+    cijena: '5,00 KM',
+  },
+  {
+    naziv: 'Ledeni čaj',
+    eng: 'Ice Tea',
+    kolicina: '0,20l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Pago',
+    eng: '',
+    kolicina: '0,20l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Prirodni sok limun',
+    eng: 'Natural juices lemon',
+    kolicina: '0,30l',
+    cijena: '3,00 KM',
+  },
+  {
+    naziv: 'Prirodni sok grejp',
+    eng: 'Natural juices grapefruit',
+    kolicina: '0,30l',
+    cijena: '4,00 KM',
+  },
+  {
+    naziv: 'Prirodni sok narandža',
+    eng: 'Natural juices orange',
+    kolicina: '0,30l',
+    cijena: '4,00 KM',
+  },
+  {
+    naziv: 'Prirodni sok nar',
+    eng: 'Natural juices pomegranate',
+    kolicina: '0,30l',
+    cijena: '4,00 KM',
+  },
+  {
+    naziv: 'Cedevita',
+    eng: '',
+    kolicina: '0,25l',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Mineralna voda',
+    eng: 'Mineral Water',
+    kolicina: '0,20l',
+    cijena: '1,50 KM',
+  },
+  {
+    naziv: 'Mineralna voda',
+    eng: 'Mineral Water',
+    kolicina: '1l',
+    cijena: '5,00 KM',
+  },
+  {
+    naziv: 'Jana',
+    eng: '',
+    kolicina: '0,20l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Jana limun',
+    eng: 'Jana lemon',
+    kolicina: '0,20l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Jana narandža',
+    eng: 'Jana orange',
+    kolicina: '0,20l',
+    cijena: '2,50 KM',
+  },
+];
+
+export const vina = [
+  {
+    naziv: ' Gemišt Graševina',
+    eng: 'Grasevina and mineral water',
+    kolicina: '0,20l',
+    cijena: '3,00 KM',
+  },
+  {
+    naziv: 'Graševina',
+    eng: '',
+    kolicina: '0,20l',
+    cijena: '6,00 KM',
+  },
+  {
+    naziv: 'Graševina',
+    eng: '',
+    kolicina: '1l',
+    cijena: '25,00 KM',
+  },
+  {
+    naziv: 'Graševina',
+    eng: '',
+    kolicina: '1l',
+    cijena: '25,00 KM',
+  },
+  {
+    naziv: 'Vranac',
+    eng: '',
+    kolicina: '1l',
+    cijena: '20,00 KM',
+  },
+  {
+    naziv: 'Crveno vino',
+    eng: 'Red wine',
+    kolicina: '1l',
+    cijena: '20,00 KM',
+  },
+  {
+    naziv: 'Crno Pro Corde',
+    eng: '',
+    kolicina: '1l',
+    cijena: '35,00 KM',
+  },
+  {
+    naziv: 'Crno Pro Corde',
+    eng: '',
+    kolicina: '0,20l',
+    cijena: '7,00 KM',
+  },
+  {
+    naziv: 'Bambus',
+    eng: 'Wine and Cola',
+    kolicina: '0,20l',
+    cijena: '3,00 KM',
+  },
+  {
+    naziv: 'Chardonnay Andrija',
+    eng: '',
+    kolicina: '0,75l',
+    cijena: '35,00 KM',
+  },
+  {
+    naziv: 'Vino Andrija Blatina - Vrhunsko',
+    eng: '',
+    kolicina: '0,75l',
+    cijena: '30,00 KM',
+  },
+  {
+    naziv: 'Vino Andrija Žilavka - Vrhunsko',
+    eng: '',
+    kolicina: '0,75l',
+    cijena: '30,00 KM',
+  },
+];
+
+export const beers = [
+  {
+    naziv: 'Bavaria',
+    eng: '',
+    kolicina: '0,25l',
+    cijena: '3,00 KM',
+  },
+  {
+    naziv: 'Staropramen',
+    eng: '',
+    kolicina: '0,33l',
+    cijena: '3,00 KM',
+  },
+  {
+    naziv: 'Ožujsko',
+    eng: '',
+    kolicina: '0,33l',
+    cijena: '3,00 KM',
+  },
+  {
+    naziv: 'Heineken',
+    eng: '',
+    kolicina: '0,33l',
+    cijena: '3,50 KM',
+  },
+  {
+    naziv: `Beck's`,
+    eng: '',
+    kolicina: '0,33l',
+    cijena: '3,00 KM',
+  },
+  {
+    naziv: 'Točeno pivo',
+    eng: 'Draught Beer',
+    kolicina: '0,25l',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Somersby',
+    eng: '',
+    kolicina: '0,33l',
+    cijena: '4,00 KM',
+  },
+  {
+    naziv: 'Bezalkoholno pivo',
+    eng: 'Nonalkoholic beer',
+    kolicina: '0,33l',
+    cijena: '3,00 KM',
+  },
+];
+
+export const spirits = [
+  {
+    naziv: 'Vlahovo',
+    eng: 'Vlahov liquer',
+    kolicina: '0,03l',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Pelinkovac',
+    eng: 'Pelinkovac liquer',
+    kolicina: '0,03l',
+    cijena: '2,00 KM',
+  },
+
+  {
+    naziv: 'Loza',
+    eng: 'Loza rakia(spirits)',
+    kolicina: '0,03l',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Travarica',
+    eng: 'Travarica rakia(spirits)',
+    kolicina: '0,03l',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Votka',
+    eng: 'Vodka',
+    kolicina: '0,03l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Tequila',
+    eng: '',
+    kolicina: '0,03l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Rum',
+    eng: '',
+    kolicina: '0,03l',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Stock',
+    eng: 'Stock brandy',
+    kolicina: '0,03l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Jeger',
+    eng: 'Jägermeister liquer',
+    kolicina: '0,03l',
+    cijena: '3,00 KM',
+  },
+
+  {
+    naziv: 'Džin',
+    eng: 'Gin',
+    kolicina: '0,03l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Orahovac',
+    eng: 'Orahovac liquer',
+    kolicina: '0,03l',
+    cijena: '2,00 KM',
+  },
+  {
+    naziv: 'Višnja',
+    eng: 'Cherry liquer',
+    kolicina: '0,03l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Aronija',
+    eng: 'Chokeberry liquer',
+    kolicina: '0,03l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'AroMedovačanija',
+    eng: 'Honig liquer',
+    kolicina: '0,03l',
+    cijena: '2,50 KM',
+  },
+  {
+    naziv: 'Viljamovka',
+    eng: '',
+    kolicina: '0,03l',
+    cijena: '3,00 KM',
+  },
+];
+export const whiskey = [
+  {
+    naziv: 'Chivas',
+    eng: '',
+    kolicina: '0,03l',
+    cijena: '5,00 KM',
+  },
+  {
+    naziv: `Jack Daniel's`,
+    eng: '',
+    kolicina: '0,03l',
+    cijena: '5,00 KM',
+  },
+  {
+    naziv: 'Johnnie Walker Red Label',
+    eng: '',
+    kolicina: '0,03l',
+    cijena: '4,00 KM',
+  },
+  {
+    naziv: 'Johnnie Walker Black Label',
+    eng: '',
+    kolicina: '0,03l',
+    cijena: '6,00 KM',
+  },
+  {
+    naziv: 'Ballantines',
+    eng: '',
+    kolicina: '0,03l',
+    cijena: '4,00 KM',
+  },
+];
